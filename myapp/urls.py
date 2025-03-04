d7979280ec1abc8e34d17f976b38b268101da078
Mr-LOCKe07 <blsmutwiri@gmail.com>
2025-03-04 13:18:40 +0300

    New Commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,8 @@ from myapp import views
 
 urlpatterns = [
     path('', views.user_list),
-    path('Add/', views.Adduser)
+    path('Add/', views.Adduser),
+    path('Edit/<id>', views.EditUser),
+    path('Delete/<id>', views.DeleteUser),
+    path('View/<id>', views.ViewUser)
 ]
